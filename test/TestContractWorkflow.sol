2c842f0bbd19ac7ace47d286a39ac7ca7d46323c
Eduard Hez <eduard.hez@maibornwolff.de>
2019-05-20 17:03:59 +0200

    Update test

--- test/TestContractWorkflow.sol
+++ test/TestContractWorkflow.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 
-import "../contracts/ENSInfrastructureInterface.sol";
+import "../contracts/AbstractENSInfrastructure.sol";
 
 
 contract TestContractWorkflow {
